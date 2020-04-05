674c41454f12ede09b595f1e1f17bc7738f33a53
Juanlu <jozaez@gmail.com>
2020-04-05 12:45:04 +0200

    Add docker-compose.yml and api-examples.zsh

---
+++ elasticsearch/api-examples.zsh
@@ -0,0 +1,32 @@
+### API BASICS
+
+# CREATE document: it also creates an index called home
+curl  --header "Content-Type: application/json" \
+      --request PUT \
+      --data '{"level":"ERROR","message":"NullPointerException...."}' \
+      http://localhost:9200/home/_doc/1 \
+      | jq
+
+curl  --header "Content-Type: application/json" \
+      --request PUT \
+      --data '{"level":"INFO","message":"User created"}' \
+      http://localhost:9200/home/_doc/2 \
+      | jq
+
+# GET document
+curl --request GET http://localhost:9200/home/_doc/1 | jq
+
+# Index metadata
+curl --request GET http://localhost:9200/home | jq
+
+# QUERY documents
+## Query param
+# shellcheck disable=SC1001
+curl --request GET http://localhost:9200/home/_search\?q\=level:ERROR | jq
+
+## Json body param
+curl  --header "Content-Type: application/json" \
+      --request GET \
+      --data '{"query": { "query_string": {  "fields" : ["level"], "query": "ERROR or INFO" } } }' \
+      http://localhost:9200/home/_search | jq
+
